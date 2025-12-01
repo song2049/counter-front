543ade296cfe819e440f5973c6ec216be4066d74
song2049 <song912049@gmail.com>
2025-12-01 13:57:01 +0900

    main

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
 import ButtonPlay from './playground/ButtonPlay';
+import Main from "./playground/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ButtonPlay/>}/>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/counter" element={<ButtonPlay/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/playground/Main.jsx
@@ -0,0 +1,7 @@
+const Main = () => {
+    return(
+        <div>나는 메인</div>
+    )
+}
+
+export default Main;
